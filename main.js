bc167ff4f298fb832d561ead3050dc42f2bc8ecd
Malte <wombatconrad@gmail.com>
2022-03-08 18:31:42 +0100

    vue basic

---
+++ main.js
@@ -0,0 +1,15 @@
+Vue.createApp({
+  data() {
+    return {
+      count: 0
+    }
+},
+
+  methods: {
+
+  },
+
+//run this code after the vue app has started
+  mounted: async function(){
+    //ladda in övningarna här inne AWAIT
+  }}).mount("body");
