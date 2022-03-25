fbe23aaf1d591364d68bfd46ca374db146489188
Malte <wombatconrad@gmail.com>
2022-03-25 14:37:27 +0100

    next

--- main.js
+++ main.js
@@ -1,196 +1,209 @@
 Vue.createApp({
-data() {
-  return {
-    readymades: [],
-    exercises: [],
-    userWorkout: [],
-    workoutTimeLeft: '',
-    exerciseTimeLeft: 45,
-    workoutActive: false
-  }
-},
-
-watch: {
-  //anytime userWorkout changes, this method runs, and addToUserWorkout
-  //the userWorkout to localStorage
-  userWorkout: {
-    handler() {
-      localStorage.userWorkout =  JSON.stringify(this.userWorkout);
-    },
-    deep: true,
-  }
-},
-
-methods: {
-  //adds the exercise to the userWorkout-array
-  addExToUserWorkout(exercise) {
-    //if there is already an exercise added, it adds a break first
-    if(this.userWorkout.length > 0){
-      let temp = { name:"Break" };
-
-      this.userWorkout.push(temp);
+  data() {
+    return {
+      readymades: [],
+      exercises: [],
+      userWorkout: [],
+      workoutTimeLeft: '',
+      workoutStopTime: 0,
+      exerciseTimeLeft: 45,
+      workoutActive: true
     }
-
-    this.userWorkout.push(exercise);
   },
 
-  addProgramToUserWorkout(workout){
-    this.userWorkout = [];
+  watch: {
+    //anytime userWorkout changes, this method runs, and addToUserWorkout
+    //the userWorkout to localStorage
+    userWorkout: {
+      handler() {
+        localStorage.userWorkout =  JSON.stringify(this.userWorkout);
+      },
+      deep: true,
+    }
+  },
 
-    for(item of workout){
+  methods: {
+    //adds the exercise to the userWorkout-array
+    addExToUserWorkout(exercise) {
       //if there is already an exercise added, it adds a break first
       if(this.userWorkout.length > 0){
-        let temp = { name:"Break" };
+        let temp = { name:"Break", length: 15 };
 
         this.userWorkout.push(temp);
       }
 
-      this.userWorkout.push(item);
-    }
-  },
+      this.userWorkout.push(exercise);
+    },
 
-  //removes a specified exercise from the userWorkout-array
-  removeExercise(exercise, index) {
-    this.userWorkout.splice(index, 1);
-  },
+    addProgramToUserWorkout(workout){
+      this.userWorkout = [];
 
-  //removes all exercises from the array
-  clearProgram() {
-    this.userWorkout = [];
-    this.exerciseTimeLeft = '';
-  },
+      for(item of workout){
+        //if there is already an exercise added, it adds a break first
+        if(this.userWorkout.length > 0){
+          let temp = { name:"Break", length: 15 };
 
-  async startWorkout() {
-    this.workoutActive = true;
-
-    let workout = document.querySelectorAll("#workout ul li");
-
-    //gets the miliseconds from some date and calculates when the workout
-    //will end (in miliseconds)
-    let currentTime = new Date().getTime();
-    let workoutStopTime = currentTime
-    + (this.userWorkout.length * (45 + 15) - 15) * 1000;
-
-
-    //removes the removeButton from the userProgram list items
-    for (item of workout) {
-      item.querySelector(".removeButton").hidden = true;
-    }
-
-    //loops through all exercises
-    for (item of workout) {
-      while(this.workoutActive) {
-
-        //sätter klassen aktiv och visar timern
-        item.classList.add("active");
-        item.querySelector(".exerciseTimer").hidden = false;
-
-        //the time the exercise or break will take
-        let timeToAdd;
-
-        if(item.querySelector('h3').textContent.includes('Break')){
-          timeToAdd = 15000;
+          this.userWorkout.push(temp);
         }
 
-        else{
-          timeToAdd = 45000;
-        }
+        this.userWorkout.push(item);
+      }
+    },
 
-        currentTime = new Date().getTime();
+    //removes a specified exercise from the userWorkout-array
+    removeExercise(exercise, index) {
+      this.userWorkout.splice(index, 1);
+    },
 
-        //calculates when the exercise will end, and the difference between
-        //that time and current time. same for workout
-        this.workoutTimeLeft = workoutStopTime - currentTime;
-        let exerciseStopTime = currentTime + timeToAdd;
-        this.exerciseTimeLeft = exerciseStopTime - currentTime;
+    //removes all exercises from the array
+    clearUserWorkout() {
+      this.userWorkout = [];
 
-        //will loop through the exercise until it reaches 0
+      this.stopWorkout();
+    },
+
+    async startWorkout() {
+      document.getElementById('start').hidden = true;
+      document.getElementById('next').hidden = false;
+
+      this.workoutActive = true;
+      let workout = document.querySelectorAll("#workout ul li");
+
+      //removes the removeButton from the userProgram list items
+      for (item of workout) {
+        item.querySelector(".removeButton").hidden = true;
+      }
+
+      //calculates the stop time
+      let timeToAdd = 0;
+
+      for(item of this.userWorkout){
+        timeToAdd = timeToAdd + item.length;
+      }
+
+      let currentTime = new Date().getTime();
+      this.workoutStopTime = currentTime + timeToAdd * 1000;
+
+      //loops through all exercises
+      for (item of workout) {
+        if(this.workoutActive){
+
+          //sätter klassen aktiv och visar timern
+          item.classList.add("active");
+          item.querySelector(".exerciseTimer").hidden = false;
+
+          if(item.querySelector('h3').textContent.includes('Break')){
+            timeToAdd = 15000;
+          }
+
+          else{
+            timeToAdd = 45000;
+          }
+
+          //calculates how many seconds of the exercise are left
+          currentTime = new Date().getTime();
+          let exerciseStopTime = currentTime + timeToAdd;
+          this.exerciseTimeLeft = exerciseStopTime - currentTime;
+
+          //will loop through the exercise until it reaches 0
+          //each time calculating the amount of time left
           while(this.exerciseTimeLeft > 0) {
-          this.exerciseTimeLeft = Math.round(
-            ((exerciseStopTime - new Date().getTime()) / 1000));
+            this.exerciseTimeLeft = Math.round(
+              ((exerciseStopTime - new Date().getTime()) / 1000));
 
-            this.workoutTimeLeft = Math.round(
-              (workoutStopTime - new Date().getTime()) / 1000);
+              this.workoutTimeLeft = Math.round(
+                (this.workoutStopTime - new Date().getTime()) / 1000);
 
-              //must wait 1 sec
-              await new Promise(resolve => setTimeout(resolve, 1000));
-            }
+                //must wait 1 sec
+                await new Promise(resolve => setTimeout(resolve, 1000));
+              }
 
-            //removes active and hides the timer
-            item.classList.remove("active");
+              //removes active and hides the timer
+              item.classList.remove("active");
+              item.querySelector(".exerciseTimer").hidden = true;
+            }//if
+          }//for
+
+          document.getElementById('start').hidden = false;
+          document.getElementById('next').hidden = true;
+          this.workoutTimeLeft = '';
+        },//method
+
+        nextExercise() {
+          this.workoutStopTime = this.workoutStopTime - (this.exerciseTimeLeft * 1000);
+          this.exerciseTimeLeft = 0;
+          console.log(this.workoutStopTime);
+        },
+
+        stopWorkout() {
+          this.workoutActive = false;
+          this.exerciseTimeLeft = 0;
+          this.workoutTimeLeft = '';
+
+          let workout = document.querySelectorAll("#workout ul li");
+
+          for (item of workout) {
+            item.querySelector(".removeButton").hidden = false;
             item.querySelector(".exerciseTimer").hidden = true;
-}//while
-        }//for
-      },//method
+            item.classList.remove("active");
+          }
+        },
 
-      stopWorkout() {
-        this.workoutActive = false;
-        this.exerciseTimeLeft = 0;
-        this.workoutTimeLeft = '';
+        startDragWorkout(evt, item){
+          evt.dataTransfer.dropEffect = "move"
+          evt.dataTransfer.effectAllowed = "move"
+          evt.dataTransfer.setData('itemID', item.id)
+          evt.dataTransfer.setData('isList', false)
+        },
 
-        let workout = document.querySelectorAll("#workout ul li");
+        startDragProgram(evt, item){
+          evt.dataTransfer.dropEffect = "move"
+          evt.dataTransfer.effectAllowed = "move"
+          evt.dataTransfer.setData('itemID', item.id)
+          evt.dataTransfer.setData('isList', true)
+        },
 
-        for (item of workout) {
-          item.querySelector(".removeButton").hidden = false;
-          item.querySelector(".exerciseTimer").hidden = true;
-          item.classList.remove("active");
-        }
-      },
-      startWorkoutDrag(evt, item){
-        evt.dataTransfer.dropEffect = "move"
-        evt.dataTransfer.effectAllowed = "move"
-        evt.dataTransfer.setData('itemID', item.id)
-        evt.dataTransfer.setData('isList', false)
-      },
+        onDrop(evt){
+          const itemID = evt.dataTransfer.getData("itemID")
+          const isList = evt.dataTransfer.getData('isList')
 
-      startProgramDrag(evt, item){
-        evt.dataTransfer.dropEffect = "move"
-        evt.dataTransfer.effectAllowed = "move"
-        evt.dataTransfer.setData('itemID', item.id)
-        evt.dataTransfer.setData('isList', true)
-      },
+          if(isList === "true") {
+            this.userWorkout = [];
 
-      onDrop(evt){
-        const itemID = evt.dataTransfer.getData("itemID")
-        const isList = evt.dataTransfer.getData('isList')
+            const list = this.readymades.find(list => list.id == itemID);
 
-        if(isList === "true") {
-          this.userWorkout = [];
-
-          const list = this.readymades.find(list => list.id == itemID);
-
-          for(item of list.content){
+            for(item of list.content){
               //if there is already an exercise added, it adds a break first
               if(this.userWorkout.length > 0){
-                let temp = { name:"Break" };
+                let temp = { name:"Break", length: 15 };
 
                 this.userWorkout.push(temp);
               }
 
               this.userWorkout.push(item);
             }//for
-        }//if
+          }//if
 
-        else {
-          const item = this.exercises.find(item => item.id == itemID)
-          this.userWorkout.push(item);
+          else {
+            const item = this.exercises.find(item => item.id == itemID)
+            this.userWorkout.push(item);
+          }
+        }//method
+      },
+
+
+      //run this code after the vue app has started
+      mounted: async function () {
+        let response = await fetch('exercises.json');
+        let json = await response.json();
+        this.exercises = json;
+
+        response = await fetch('readymade.json');
+        json = await response.json();
+        this.readymades = json;
+
+        if (localStorage.userWorkout) {
+          this.userWorkout = JSON.parse(localStorage.userWorkout);
         }
-      }//method
-    },
-
-
-    //run this code after the vue app has started
-    mounted: async function () {
-      let response = await fetch('exercises.json');
-      let json = await response.json();
-      this.exercises = json;
-
-      response = await fetch('readymade.json');
-      json = await response.json();
-      this.readymades = json;
-
-      if (localStorage.userWorkout) {
-        this.userWorkout = JSON.parse(localStorage.userWorkout);
       }
-    }
-  }).mount("body");
+    }).mount("body");
